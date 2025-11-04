cc7532ea23a45f675534d6528f8d8df22bc846ab
Killian Godet <godetkillian@outlook.com>
2025-11-04 19:51:08 +0100

    fix(utilisateur): Ajout de migrations

---
+++ src/EventListener/UserPasswordEncoderListener.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\EventListener;
+
+use App\Entity\User;
+use Doctrine\Bundle\DoctrineBundle\Attribute\AsDoctrineListener;
+use Doctrine\ORM\Event\PreUpdateEventArgs;
+use Doctrine\ORM\Event\PrePersistEventArgs;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+#[AsDoctrineListener(event: 'prePersist')]
+#[AsDoctrineListener(event: 'preUpdate')]
+class UserPasswordEncoderListener
+{
+    public function __construct(private UserPasswordHasherInterface $hasher) {}
+
+    public function prePersist(PrePersistEventArgs $args): void
+    {
+        $this->encodePassword($args->getObject());
+    }
+
+    public function preUpdate(PreUpdateEventArgs $args): void
+    {
+        $this->encodePassword($args->getObject());
+    }
+
+    private function encodePassword(object $entity): void
+    {
+        if (!$entity instanceof User || null === $entity->getPlainPassword()) {
+            return;
+        }
+
+        $entity->setPassword(
+            $this->hasher->hashPassword($entity, $entity->getPlainPassword())
+        );
+        $entity->erasePlainPassword();
+    }
+}
