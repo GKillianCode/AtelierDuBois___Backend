e16185675cf9a2c5ecc0fa4c6e6922ad088be754
Killian Godet <godetkillian@outlook.com>
2025-11-04 19:54:40 +0100

    fix(utilisateur): Encodage du mot de passe

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -6,13 +6,17 @@ use App\Entity\User;
 use Doctrine\Persistence\ObjectManager;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Symfony\Component\Validator\Validator\ValidatorInterface;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class AppFixtures extends Fixture
 {
     private ValidatorInterface $validator;
-    public function __construct(ValidatorInterface $validator)
+    private UserPasswordHasherInterface $encoder;
+
+    public function __construct(ValidatorInterface $validator, UserPasswordHasherInterface $encoder)
     {
         $this->validator = $validator;
+        $this->encoder = $encoder;
     }
 
     public function load(ObjectManager $manager): void
@@ -21,7 +25,7 @@ class AppFixtures extends Fixture
         $user = new User();
         $user->setUuid('123e4567-e89b-12d3-a456-426614174000');
         $user->setEmail('john.doe@example.com');
-        $user->setPassword('123456789');
+        $user->setPlainPassword('Abricot2024!');
         $user->setFirstname('John');
         $user->setLastname('Doe');
         $user->setBirthDate(new \DateTimeImmutable('1990-01-01'));
@@ -29,7 +33,7 @@ class AppFixtures extends Fixture
         $user->setCreatedAt(new \DateTimeImmutable());
         $user->setUpdatedAt(new \DateTimeImmutable());
 
-        $errors = $this->validator->validate($user);
+        $errors = $this->validator->validate($user, null, ['registration']);
 
         if (count($errors) > 0) {
             foreach ($errors as $error) {
