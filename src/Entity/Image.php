09c92b5091f4b071a95a925c23d3a23f3eefc1bc
Killian Godet <godetkillian@outlook.com>
2025-11-13 19:08:28 +0100

    feat: Ajout de l'entité Image

---
+++ src/Entity/Image.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ImageRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ImageRepository::class)]
+class Image
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 50)]
+    private ?string $folderName = null;
+
+    #[ORM\Column(length: 10)]
+    private ?string $imageName = null;
+
+    #[ORM\ManyToOne(inversedBy: 'images')]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?ProductVariant $productVariantId = null;
+
+    #[ORM\Column]
+    private ?\DateTimeImmutable $createdAt = null;
+
+    #[ORM\Column]
+    private ?\DateTimeImmutable $updatedAt = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getFolderName(): ?string
+    {
+        return $this->folderName;
+    }
+
+    public function setFolderName(string $folderName): static
+    {
+        $this->folderName = $folderName;
+
+        return $this;
+    }
+
+    public function getImageName(): ?string
+    {
+        return $this->imageName;
+    }
+
+    public function setImageName(string $imageName): static
+    {
+        $this->imageName = $imageName;
+
+        return $this;
+    }
+
+    public function getProductVariantId(): ?ProductVariant
+    {
+        return $this->productVariantId;
+    }
+
+    public function setProductVariantId(?ProductVariant $productVariantId): static
+    {
+        $this->productVariantId = $productVariantId;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeImmutable
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(\DateTimeImmutable $createdAt): static
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    public function getUpdatedAt(): ?\DateTimeImmutable
+    {
+        return $this->updatedAt;
+    }
+
+    public function setUpdatedAt(\DateTimeImmutable $updatedAt): static
+    {
+        $this->updatedAt = $updatedAt;
+
+        return $this;
+    }
+}
