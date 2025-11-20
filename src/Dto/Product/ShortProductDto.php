159158057faaab80280ab50848facbec8d7546c1
Killian Godet <godetkillian@outlook.com>
2025-11-20 16:39:03 +0100

    refactor: Remplacement de unitPrice, mainImage et publicId par leurs Dtos

--- src/Dto/Product/ShortProductDto.php
+++ src/Dto/Product/ShortProductDto.php
@@ -22,45 +22,10 @@ class ShortProductDto
         )]
         public readonly string $title,
 
-        #[Assert\Type(
-            type: 'int',
-            message: 'La valeur {{ value }} n\'est pas valide pour le prix unitaire.'
-        )]
-        #[Assert\GreaterThan(
-            value: 0,
-            message: 'Le prix unitaire doit être supérieur à 0.'
-        )]
-        public readonly ?int $unitPrice,
+        public readonly ?PriceDto $unitPrice,
 
-        #[Assert\Type(
-            type: 'string',
-            message: 'La valeur {{ value }} n\'est pas valide pour l\'URL de l\'image.'
-        )]
-        #[Assert\NotBlank(
-            message: 'L\'URL de l\'image ne doit pas être vide.'
-        )]
-        #[Assert\Url(
-            message: 'L\'URL de l\'image n\'est pas une URL valide.'
-        )]
-        #[Assert\Length(
-            max: 255,
-            maxMessage: 'L\'URL de l\'image ne doit pas dépasser {{ limit }} caractères.'
-        )]
-        public readonly string $mainImageUrl,
+        public readonly ImageDto $mainImage,
 
-        #[Assert\Type(
-            type: 'string',
-            message: 'La valeur {{ value }} n\'est pas valide pour le publicId.'
-        )]
-        #[Assert\NotBlank(
-            message: 'Le publicId ne doit pas être vide.'
-        )]
-        #[Assert\Length(
-            min: 1,
-            max: 100,
-            minMessage: 'Le publicId doit contenir au minimum {{ limit }} caractères.',
-            maxMessage: 'Le publicId ne doit pas dépasser {{ limit }} caractères.'
-        )]
-        public readonly string $publicId
+        public readonly PublicIdDto $publicId,
     ) {}
 }
