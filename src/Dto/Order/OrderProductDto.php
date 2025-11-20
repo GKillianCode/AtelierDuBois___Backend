3d65e617fe5f01f490c8cf59a60f4b501fb9c2ec
Killian Godet <godetkillian@outlook.com>
2025-11-20 16:35:14 +0100

    refactor: Ajout de validation min sur la taille du texte de productName & woodType et remplacement de publicId par le Dto PublicId

--- src/Dto/Order/OrderProductDto.php
+++ src/Dto/Order/OrderProductDto.php
@@ -2,20 +2,14 @@
 
 namespace App\Dto\Order;
 
+use App\Dto\Product\PriceDto;
+use App\Dto\Product\PublicIdDto;
 use Symfony\Component\Validator\Constraints as Assert;
 
 class OrderProductDto
 {
     public function __construct(
-        #[Assert\Type(
-            type: 'int',
-            message: 'La valeur {{ value }} n\'est pas valide pour le prix.'
-        )]
-        #[Assert\GreaterThan(
-            value: 0,
-            message: 'Le prix unitaire doit être supérieur à 0.'
-        )]
-        public readonly int $unitPrice,
+        public readonly ?PriceDto $unitPrice,
 
         #[Assert\Type(
             type: 'integer',
@@ -32,6 +26,8 @@ class OrderProductDto
             message: 'La valeur {{ value }} n\'est pas valide pour le nom du produit'
         )]
         #[Assert\Length(
+            min: 3,
+            minMessage: 'Le nom du produit doit contenir au moins {{ limit }} caractères.',
             max: 150,
             maxMessage: 'Le nom du produit ne doit pas dépasser {{ limit }} caractères.'
         )]
@@ -40,24 +36,15 @@ class OrderProductDto
         )]
         public readonly string $productName,
 
-        #[Assert\Type(
-            type: 'string',
-            message: 'La valeur {{ value }} n\'est pas valide pour l\'URL publique.'
-        )]
-        #[Assert\Length(
-            max: 100,
-            maxMessage: 'L\'URL publique ne doit pas dépasser {{ limit }} caractères.'
-        )]
-        #[Assert\NotBlank(
-            message: 'L\'URL publique ne doit pas être vide.'
-        )]
-        public readonly string $publicUrl,
+        public readonly PublicIdDto $publicId,
 
         #[Assert\Type(
             type: 'string',
             message: 'La valeur {{ value }} n\'est pas valide pour le type de bois.'
         )]
         #[Assert\Length(
+            min: 2,
+            minMessage: 'Le nom du bois doit contenir au moins {{ limit }} caractères.',
             max: 100,
             maxMessage: 'Le nom du bois ne doit pas dépasser {{ limit }} caractères.'
         )]
