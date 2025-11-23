25b6a038ac0784384a1334d1898d85be2d3781a9
Killian Godet <godetkillian@outlook.com>
2025-11-23 22:19:28 +0100

    fix: Remplacement de l'identifier de uuid à email

--- src/Entity/User/User.php
+++ src/Entity/User/User.php
@@ -147,7 +147,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
      */
     public function getUserIdentifier(): string
     {
-        return (string) $this->uuid;
+        return (string) $this->email;
     }
 
     /**
