d8ed39c4280165d3c875804b2e847947b6a0ae23
Killian Godet <godetkillian@outlook.com>
2025-11-23 21:46:03 +0100

    feat(Add-address): Ajout d'un service pour les addresses et une méthode d'ajout d'address

--- src/Service/User/AddressService.php
+++ src/Service/User/AddressService.php
@@ -2,24 +2,131 @@
 
 namespace App\Service\User;
 
+use App\Entity\User\User;
 use App\Dto\User\AddressDto;
+use App\Entity\User\Address;
 use App\Service\UuidService;
+use Psr\Log\LoggerInterface;
+use App\Entity\User\AddressType;
+use App\Service\ValidatorService;
+use Doctrine\ORM\EntityManagerInterface;
+use App\Repository\User\AddressTypeRepository;
+use Symfony\Component\DependencyInjection\Attribute\Autowire;
 
 class AddressService
 {
-    private UuidService $uuidService;
 
-    public function __construct(UuidService $uuidService)
+    public function __construct(
+        public readonly LoggerInterface $logger,
+        public readonly EntityManagerInterface $entityManager,
+        public readonly UuidService $uuidService,
+        public readonly AddressTypeRepository $addressTypeRepository,
+        public readonly ValidatorService $validatorService,
+        #[Autowire('%env(int:USER_MAX_ADDRESSES)%')]
+        private readonly int $userMaxAddresses = 5
+    ) {}
+
+    /**
+     * Check if the user can add a new address based on the maximum allowed addresses.
+     * @param User $user
+     * @return bool
+     */
+    public function canAddAddress(User $user): bool
     {
-        $this->uuidService = $uuidService;
+        $this->logger->debug("AddressService::canAddAddress ENTER");
+        $canAdd = $user->getAddresses()->count() < $this->userMaxAddresses;
+        $this->logger->debug("AddressService::canAddAddress EXIT");
+        return $canAdd;
     }
 
-    public function addAddress(AddressDto $addressDto): void
+    /**
+     * Add a new address for the user based on the provided AddressDto.
+     * @param AddressDto $addressDto
+     * @param User $user
+     * @return void
+     */
+    public function addAddress(AddressDto $addressDto, User $user): void
     {
-        $newUuid = $this->uuidService->generateUuid62();
+        $this->logger->debug("AddressService::addAddress ENTER");
+
+        $address = $this->createAddressFromDto($addressDto, $user);
+        $this->validateAddress($address);
+        $this->persistAddress($address);
+
+        $this->logger->debug("AddressService::addAddress EXIT");
     }
 
-    public function getAddress(int $id): void {}
+    /**
+     * Create an Address entity from the provided AddressDto and User.
+     * @param AddressDto $addressDto
+     * @param User $user
+     * @return Address
+     */
+    private function createAddressFromDto(AddressDto $addressDto, User $user): Address
+    {
+        $this->logger->debug("AddressService::createAddressFromDto ENTER");
 
-    public function getAllAddress(): void {}
+        $newUuidBase62 = $this->uuidService->generateUuid62();
+        $addressType = $this->getAddressTypeById($addressDto->isProfessionnalAddress ? 2 : 1);
+
+        $address = new Address();
+        $address->setUserId($user)
+            ->setPublicId($newUuidBase62)
+            ->setStreet($addressDto->street)
+            ->setZipcode($addressDto->zipcode)
+            ->setCity($addressDto->city)
+            ->setAddressTypeId($addressType)
+            ->setCreatedAt(new \DateTimeImmutable())
+            ->setUpdatedAt(new \DateTimeImmutable());
+
+        $this->logger->debug("AddressService::createAddressFromDto EXIT");
+        return $address;
+    }
+
+    /**
+     * Validate the Address entity.
+     * @param Address $address
+     * @return void
+     */
+    private function validateAddress(Address $address): void
+    {
+        $this->logger->debug("AddressService::validateAddress ENTER");
+
+        $violations = $this->validatorService->getViolationsAsArray($address);
+
+        if (!empty($violations)) {
+            $this->logger->error("AddressService::validateAddress VALIDATION ERROR");
+            throw new \RuntimeException('Validation error while adding address: ' . json_encode($violations));
+        }
+
+        $this->logger->debug("AddressService::validateAddress EXIT");
+    }
+
+    /**
+     * Persist the Address entity to the database.
+     * @param Address $address
+     * @return void
+     */
+    private function persistAddress(Address $address): void
+    {
+        $this->logger->debug("AddressService::persistAddress ENTER");
+
+        $this->entityManager->persist($address);
+        $this->entityManager->flush();
+
+        $this->logger->debug("AddressService::persistAddress EXIT");
+    }
+
+    /**
+     * Retrieve AddressType entity by its ID.
+     * @param int $id
+     * @return AddressType|null
+     */
+    public function getAddressTypeById(int $id): ?AddressType
+    {
+        $this->logger->debug("AddressService::getAddressTypeById ENTER");
+        $addressType = $this->addressTypeRepository->findOneBy(['id' => $id]);
+        $this->logger->debug("AddressService::getAddressTypeById EXIT");
+        return $addressType;
+    }
 }
