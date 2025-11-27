8048996d06692d8ddebb4280bd9ee337b8154b0b
Killian Godet <godetkillian@outlook.com>
2025-11-27 21:35:43 +0100

    refactor: Suppression de commentaire d'exemple

--- src/Repository/Product/ProductVariantRepository.php
+++ src/Repository/Product/ProductVariantRepository.php
@@ -15,29 +15,4 @@ class ProductVariantRepository extends ServiceEntityRepository
     {
         parent::__construct($registry, ProductVariant::class);
     }
-
-    //    /**
-    //     * @return ProductVariant[] Returns an array of ProductVariant objects
-    //     */
-    //    public function findByExampleField($value): array
-    //    {
-    //        return $this->createQueryBuilder('p')
-    //            ->andWhere('p.exampleField = :val')
-    //            ->setParameter('val', $value)
-    //            ->orderBy('p.id', 'ASC')
-    //            ->setMaxResults(10)
-    //            ->getQuery()
-    //            ->getResult()
-    //        ;
-    //    }
-
-    //    public function findOneBySomeField($value): ?ProductVariant
-    //    {
-    //        return $this->createQueryBuilder('p')
-    //            ->andWhere('p.exampleField = :val')
-    //            ->setParameter('val', $value)
-    //            ->getQuery()
-    //            ->getOneOrNullResult()
-    //        ;
-    //    }
 }
