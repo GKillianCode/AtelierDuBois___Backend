f8d114b521165523d697395964f4c03258bba9ad
Killian Godet <godetkillian@outlook.com>
2025-11-27 21:40:50 +0100

    feat: Définition de createdAt et updatedAt dans le controller et modification des valeurs des assert

--- src/Entity/Product/Image.php
+++ src/Entity/Product/Image.php
@@ -13,14 +13,17 @@ class Image
     #[ORM\Column]
     private ?int $id = null;
 
-    #[ORM\Column(length: 50)]
+    #[ORM\Column(length: 22)]
     private ?string $folderName = null;
 
-    #[ORM\Column(length: 10)]
+    #[ORM\Column(length: 22)]
     private ?string $imageName = null;
 
-    #[ORM\Column(length: 15)]
+    #[ORM\Column(length: 4)]
     private ?string $format = null;
+
+    #[ORM\Column]
+    private ?bool $isDefault = null;
 
     #[ORM\ManyToOne(inversedBy: 'images')]
     #[ORM\JoinColumn(nullable: false)]
@@ -31,6 +34,12 @@ class Image
 
     #[ORM\Column]
     private ?\DateTimeImmutable $updatedAt = null;
+
+    public function __construct()
+    {
+        $this->createdAt = new \DateTimeImmutable();
+        $this->updatedAt = new \DateTimeImmutable();
+    }
 
     public function getId(): ?int
     {
@@ -61,18 +70,6 @@ class Image
         return $this;
     }
 
-    public function getProductVariantId(): ?ProductVariant
-    {
-        return $this->productVariantId;
-    }
-
-    public function setProductVariantId(?ProductVariant $productVariantId): static
-    {
-        $this->productVariantId = $productVariantId;
-
-        return $this;
-    }
-
     public function getFormat(): ?string
     {
         return $this->format;
@@ -81,6 +78,30 @@ class Image
     public function setFormat(string $format): static
     {
         $this->format = $format;
+
+        return $this;
+    }
+
+    public function isDefault(): ?bool
+    {
+        return $this->isDefault;
+    }
+
+    public function setIsDefault(bool $isDefault): static
+    {
+        $this->isDefault = $isDefault;
+
+        return $this;
+    }
+
+    public function getProductVariantId(): ?ProductVariant
+    {
+        return $this->productVariantId;
+    }
+
+    public function setProductVariantId(?ProductVariant $productVariantId): static
+    {
+        $this->productVariantId = $productVariantId;
 
         return $this;
     }
