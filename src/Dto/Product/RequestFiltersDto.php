5420bda2e60a57e1952d3cfe1a439012b7773acc
Killian Godet <godetkillian@outlook.com>
2025-11-30 23:44:22 +0100

    feat: Regroupement des différents filtres dans un dto pour le passage en paramètre des méthodes

---
+++ src/Dto/Product/RequestFiltersDto.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Dto\Product;
+
+use App\Enum\SortFilterCode;
+
+class RequestFiltersDto
+{
+    public function __construct(
+        public string $search,
+        public SortFilterCode $filter,
+        public SortFilterCode $productType,
+    ) {}
+}
