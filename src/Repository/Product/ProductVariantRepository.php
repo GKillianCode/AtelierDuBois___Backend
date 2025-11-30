7cc9ab17bdba94f6337a34f88045f1415ae2cedd
Killian Godet <godetkillian@outlook.com>
2025-11-30 22:01:28 +0100

    feat: Ajout de la méthode getProductVariantByPublicId pour récupérer tout les variants d'un produit

--- src/Repository/Product/ProductVariantRepository.php
+++ src/Repository/Product/ProductVariantRepository.php
@@ -15,4 +15,35 @@ class ProductVariantRepository extends ServiceEntityRepository
     {
         parent::__construct($registry, ProductVariant::class);
     }
+
+    public function getProductVariantByPublicId(string $publicId): ?ProductVariant
+    {
+        $productVariant = $this->createQueryBuilder('pv')
+            ->select('pv', 'p', 'i', 'w')
+            ->leftJoin('pv.productId', 'p')
+            ->leftJoin('pv.images', 'i')
+            ->leftJoin('pv.woodId', 'w')
+            ->where('pv.publicId = :publicId')
+            ->setParameter('publicId', $publicId)
+            ->getQuery()
+            ->getOneOrNullResult();
+
+        return $productVariant;
+    }
+
+    public function getAllMinimalProductVariant(string $productId): ?array
+    {
+        $productVariants = $this->createQueryBuilder('pv')
+            ->select('pv', 'p', 'i', 'w')
+            ->leftJoin('pv.productId', 'p')
+            ->leftJoin('pv.images', 'i')
+            ->leftJoin('pv.woodId', 'w')
+            ->where('pv.productId = :productId')
+            ->andWhere('i.isDefault = true')
+            ->setParameter('productId', $productId)
+            ->getQuery()
+            ->getResult();
+
+        return $productVariants;
+    }
 }
