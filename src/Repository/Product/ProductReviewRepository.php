5b1c1a76866cef09cd86f9295ab8fe8576502d97
Killian Godet <godetkillian@outlook.com>
2026-01-04 18:54:32 +0100

    feat: Ajout de la méthode paginateProductReviews qui permet d'obtenir la liste des commentaire (rating) d'un produit avec une pagination

--- src/Repository/Product/ProductReviewRepository.php
+++ src/Repository/Product/ProductReviewRepository.php
@@ -2,42 +2,46 @@
 
 namespace App\Repository\Product;
 
+use Psr\Log\LoggerInterface;
 use App\Entity\Product\ProductReview;
-use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
+use Doctrine\ORM\Tools\Pagination\Paginator;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 
 /**
  * @extends ServiceEntityRepository<ProductReview>
  */
 class ProductReviewRepository extends ServiceEntityRepository
 {
-    public function __construct(ManagerRegistry $registry)
+    private LoggerInterface $logger;
+
+    public function __construct(ManagerRegistry $registry, LoggerInterface $logger)
     {
         parent::__construct($registry, ProductReview::class);
+        $this->logger = $logger;
     }
 
-    //    /**
-    //     * @return ProductReview[] Returns an array of ProductReview objects
-    //     */
-    //    public function findByExampleField($value): array
-    //    {
-    //        return $this->createQueryBuilder('p')
-    //            ->andWhere('p.exampleField = :val')
-    //            ->setParameter('val', $value)
-    //            ->orderBy('p.id', 'ASC')
-    //            ->setMaxResults(10)
-    //            ->getQuery()
-    //            ->getResult()
-    //        ;
-    //    }
+    /**
+     * Get a paginated list of products based on filters.
+     * @param int $page
+     * @param int $limit
+     * @param int $productVariantId
+     * @return Paginator
+     */
+    public function paginateProductReviews(int $page, int $limit, int $productId): Paginator
+    {
+        $this->logger->debug("ProductReviewRepository::paginateProductReviews ENTER with page: $page, limit: $limit, productId: $productId");
+        $query = $this->createQueryBuilder('pr')
+            ->leftJoin('pr.userId', 'u')
+            ->addSelect('u')
+            ->where('pr.productVariantId = :productVariantId')
+            ->setParameter('productVariantId', $productId)
+            ->setFirstResult(($page - 1) * $limit)
+            ->setMaxResults($limit)
+            ->getQuery();
 
-    //    public function findOneBySomeField($value): ?ProductReview
-    //    {
-    //        return $this->createQueryBuilder('p')
-    //            ->andWhere('p.exampleField = :val')
-    //            ->setParameter('val', $value)
-    //            ->getQuery()
-    //            ->getOneOrNullResult()
-    //        ;
-    //    }
+        $this->logger->debug("ProductReviewRepository::paginateProductReviews EXIT");
+
+        return new Paginator($query, true);
+    }
 }
