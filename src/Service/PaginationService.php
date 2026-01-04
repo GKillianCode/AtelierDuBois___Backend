fd5e9ef54e1209c8d4e124f780952051c040f1f2
Killian Godet <godetkillian@outlook.com>
2026-01-04 19:02:16 +0100

    feat: Ajout d'une méthode qui permet de récupérer les informations de pagination (page actuelle, total des pages..)

---
+++ src/Service/PaginationService.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Service;
+
+use Psr\Log\LoggerInterface;
+use App\Dto\Product\PaginationDataDto;
+use Doctrine\ORM\Tools\Pagination\Paginator;
+
+class PaginationService
+{
+    public function __construct(
+        private readonly LoggerInterface $logger
+    ) {}
+
+    public function getMetaPaginationData(Paginator $paginator, int $limit, int $page): PaginationDataDto
+    {
+        $this->logger->debug("PaginationService::getMetaPaginationData ENTER");
+
+        $totalItems = count($paginator);
+        $totalPages = (int) ceil($totalItems / $limit);
+
+        $paginationDataDto = new PaginationDataDto(
+            currentPage: $page,
+            totalPages: $totalPages,
+            totalItems: $totalItems,
+            itemsPerPage: $limit,
+            hasNextPage: $page < $totalPages,
+            hasPreviousPage: $page > 1
+        );
+
+        $this->logger->debug("PaginationService::getMetaPaginationData EXIT");
+
+        return $paginationDataDto;
+    }
+}
