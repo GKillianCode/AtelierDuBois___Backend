240134f0716dcdb08d332c36920ca9ff59752c41
Killian Godet <godetkillian@outlook.com>
2026-01-12 22:26:36 +0100

    feat: Ajout de order_number dans la table order

---
+++ migrations/Version20260109201734.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20260109201734 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE "order" ADD order_number VARCHAR(17) NOT NULL');
+        $this->addSql('ALTER TABLE "order" ALTER tracking_number TYPE VARCHAR(17)');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE SCHEMA public');
+        $this->addSql('ALTER TABLE "order" DROP order_number');
+        $this->addSql('ALTER TABLE "order" ALTER tracking_number TYPE VARCHAR(50)');
+    }
+}
