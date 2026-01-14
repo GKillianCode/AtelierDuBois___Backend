bd95c3dc1a902ea8ba6e40167dbdf1b7ce8e96ce
Killian Godet <godetkillian@outlook.com>
2026-01-14 19:44:13 +0100

    feat: Ajout d'un service pour les orders avec comme méthodes : getAllOrders, getAllOrdersInShortOrderDto, calculateTotalQuantity, calculateTotalAmount et generateNewOrderNumber

---
+++ src/Service/Order/OrderService.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace App\Service\Order;
+
+use App\Dto\Order\ShortOrderDto;
+use App\Entity\User\User;
+use App\Service\UuidService;
+use Psr\Log\LoggerInterface;
+use App\Service\PaginationService;
+use App\Repository\Order\OrderRepository;
+use Doctrine\ORM\Tools\Pagination\Paginator;
+
+class OrderService
+{
+    public function __construct(
+        private readonly OrderRepository $orderRepository,
+        private readonly PaginationService $paginationService,
+        private readonly UuidService $uuidService,
+        private readonly LoggerInterface $logger,
+    ) {}
+
+    public function getAllOrders(int $page, int $limit, User $user): array
+    {
+        $this->logger->debug("OrderService::getAllOrders ENTER");
+
+        $page = max(1, $page);
+        $limit = min(80, max(1, $limit));
+
+        $paginator = $this->orderRepository->paginateOrders($page, $limit, $user);
+
+        $ordersDto = $this->getAllOrdersInShortOrderDto($paginator);
+
+        $paginationDataDto = $this->paginationService->getMetaPaginationData($paginator, $limit, $page);
+
+        $this->logger->debug("OrderService::getAllOrders EXIT");
+
+        return [
+            'orders' => $ordersDto,
+            'pagination' => $paginationDataDto
+        ];
+    }
+
+    private function getAllOrdersInShortOrderDto(Paginator $paginator): array
+    {
+        $this->logger->debug("OrderService::getAllOrdersInShortOrderDto ENTER");
+
+        $products = [];
+        foreach ($paginator as $order) {
+            $products[] = new ShortOrderDto(
+                orderNumber: $this->generateNewOrderNumber(),
+                trackingNumber: $this->uuidService->generateUuid62(),
+                productCount: $this->calculateTotalQuantity($order),
+                totalAmount: $this->calculateTotalAmount($order),
+                status: $order->getStatusId()->getName(),
+                createdAt: $order->getCreatedAt()
+            );
+        }
+
+        $this->logger->debug("OrderService::getAllOrdersInShortOrderDto EXIT");
+        return $products;
+    }
+
+    private function calculateTotalQuantity($order): int
+    {
+        $this->logger->debug("OrderService::calculateTotalQuantity ENTER");
+
+        $totalQuantity = 0;
+        foreach ($order->getOrderProducts() as $orderProduct) {
+            $totalQuantity += $orderProduct->getQuantity();
+        }
+
+        $this->logger->debug("OrderService::calculateTotalQuantity EXIT");
+        return $totalQuantity;
+    }
+
+    private function calculateTotalAmount($order): int
+    {
+        $this->logger->debug("OrderService::calculateTotalAmount ENTER");
+
+        $totalAmount = 0;
+        foreach ($order->getOrderProducts() as $orderProduct) {
+            $totalAmount += $orderProduct->getQuantity() * $orderProduct->getPrice();
+        }
+
+        $this->logger->debug("OrderService::calculateTotalAmount EXIT");
+        return $totalAmount;
+    }
+
+    public function generateNewOrderNumber(): string
+    {
+        $this->logger->debug("OrderService::generateNewOrderNumber ENTER");
+
+        $yearMonth = (new \DateTimeImmutable())->format('ym');
+        $uuid = $this->uuidService->generateUuid62();
+        $last8Digits = strtoupper(str_split($uuid, 8)[1]);
+
+        $this->logger->debug("OrderService::generateNewOrderNumber EXIT");
+        return "ORD-{$yearMonth}-{$last8Digits}";
+    }
+}
