f52ca14bad377c36e4b87fcc1b6eb417f84a3128
Killian Godet <godetkillian@outlook.com>
2026-01-24 19:35:57 +0100

    feat(cleancode01): ajouter manager pour ShipmentStatus

---
+++ src/Manager/Order/ShipmentStatusManager.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Manager\Order;
+
+use App\Entity\Order\OrderStatus;
+use App\Util\ValidatorUtil;
+use Psr\Log\LoggerInterface;
+use App\Entity\Product\Category;
+use App\Enum\ShipmentStatusCode;
+use App\Trait\ValidateAndSaveTrait;
+use Doctrine\ORM\EntityManagerInterface;
+use App\Repository\Order\OrderStatusRepository;
+
+class ShipmentStatusManager
+{
+    public function __construct(
+        private readonly LoggerInterface $logger,
+        private readonly OrderStatusRepository $shipmentStatusRepository,
+        private readonly EntityManagerInterface $entityManager,
+        private readonly ValidatorUtil $validatorUtil
+    ) {}
+
+    public function create(): void
+    {
+        $this->logger->debug("ShipmentStatusManager::create ENTER");
+
+        $this->logger->debug("ShipmentStatusManager::create EXIT");
+    }
+
+    public function update(OrderStatus $shipmentStatus): void
+    {
+        $shipmentStatus->setUpdatedAt(new \DateTimeImmutable());
+        $this->entityManager->flush();
+    }
+
+    public function delete(OrderStatus $shipmentStatus): void
+    {
+        $this->entityManager->remove($shipmentStatus);
+        $this->entityManager->flush();
+    }
+
+    use ValidateAndSaveTrait;
+
+    public function getShipmentStatusByCode(ShipmentStatusCode $code): ?OrderStatus
+    {
+        $this->logger->debug("ShipmentStatusManager::create ENTER");
+        $shipmentStatusCode = $this->shipmentStatusRepository->findOneBy(['code' => $code->value]);
+        $this->logger->debug("ShipmentStatusManager::create EXIT");
+        return $shipmentStatusCode;
+    }
+}
