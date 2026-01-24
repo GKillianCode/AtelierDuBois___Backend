33eef48375d669760884012edf584819838d63bd
Killian Godet <godetkillian@outlook.com>
2026-01-24 12:32:41 +0100

    refactor(cleancode01): migré méthodes vers CategoryManager et CategoryMapper

--- src/Service/Product/CategoryService.php
+++ src/Service/Product/CategoryService.php
@@ -3,41 +3,34 @@
 namespace App\Service\Product;
 
 use Psr\Log\LoggerInterface;
-use App\Dto\Types\CategoryDto;
-use App\Dto\Types\PublicIdDto;
-use App\Repository\Product\CategoryRepository;
+use App\Mapper\Product\CategoryMapper;
+use App\Manager\Product\CategoryManager;
 
 class CategoryService
 {
     public function __construct(
-        public readonly CategoryRepository $categoryRepository,
-        public readonly LoggerInterface $logger
+        private readonly CategoryManager $categoryManager,
+        private readonly CategoryMapper $categoryMapper,
+        private readonly LoggerInterface $logger
     ) {}
 
     public function getAllCategoriesInCategoryDto(): array
     {
         $this->logger->debug("CategoryService::getAllCategoriesInCategoryDto ENTER");
-        $categories = $this->categoryRepository->findAll();
+        $categories = $this->categoryManager->getAllCategories();
         $categories = $this->categoriesToCategoriesDto($categories);
         $this->logger->debug("CategoryService::getAllCategoriesInCategoryDto EXIT");
         return $categories;
     }
 
-    public function categoryToCategoryDto($category): CategoryDto
-    {
-        $categoryDto = new CategoryDto(
-            name: $category->getName(),
-            publicId: new PublicIdDto($category->getPublicId()),
-        );
-        return $categoryDto;
-    }
-
     public function categoriesToCategoriesDto(array $categories): array
     {
+        $this->logger->debug("CategoryService::categoriesToCategoriesDto ENTER");
         $categoriesDto = [];
         foreach ($categories as $category) {
-            $categoriesDto[] = $this->categoryToCategoryDto($category);
+            $categoriesDto[] = $this->categoryMapper->toDtoFromEntity($category);
         }
+        $this->logger->debug("CategoryService::categoriesToCategoriesDto EXIT");
         return $categoriesDto;
     }
 }
