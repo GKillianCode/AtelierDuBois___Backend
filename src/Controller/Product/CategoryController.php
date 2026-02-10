05e6e3fd44558ebd2a50bf57d7f9aa395f058621
Killian Godet <godetkillian@outlook.com>
2026-02-10 19:30:22 +0100

    fix(cleancode01): utiliser SerializerInterface pour l'utilisation des serializers

--- src/Controller/Product/CategoryController.php
+++ src/Controller/Product/CategoryController.php
@@ -7,6 +7,7 @@ use Psr\Log\LoggerInterface;
 use App\Service\Product\CategoryService;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Serializer\SerializerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 final class CategoryController extends AbstractController
@@ -14,6 +15,7 @@ final class CategoryController extends AbstractController
     public function __construct(
         private readonly CategoryService $categoryService,
         public readonly LoggerInterface $logger,
+        public readonly SerializerInterface $serializer,
     ) {}
 
     #[Route('/api/public/v1/category/all', name: 'app_product_category', methods: ['GET'])]
@@ -24,7 +26,7 @@ final class CategoryController extends AbstractController
             $categoriesDto = $this->categoryService->getAllCategoriesInCategoryDto();
             $this->logger->debug("CategoryController::getAllProducts EXIT");
 
-            return ApiResponse::success($categoriesDto);
+            return ApiResponse::success($this->serializer->normalize($categoriesDto));
         } catch (\Exception $e) {
             return ApiResponse::error(
                 'An error occurred while fetching categories. ' . $e->getMessage(),
