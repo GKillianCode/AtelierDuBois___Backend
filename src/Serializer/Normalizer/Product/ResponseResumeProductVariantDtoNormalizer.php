a5a6c722556d38d2d267ee0ce7af812d333ad740
Killian Godet <godetkillian@outlook.com>
2026-02-15 18:04:11 +0100

    refactor(cleancode01): supprimer champs inutiles (champs déplacés)

--- src/Serializer/Normalizer/Product/ResponseResumeProductVariantDtoNormalizer.php
+++ src/Serializer/Normalizer/Product/ResponseResumeProductVariantDtoNormalizer.php
@@ -2,8 +2,6 @@
 
 namespace App\Serializer\Normalizer\Product;
 
-use App\Dto\Product\ProductDetailDto;
-use App\Dto\Response\ResponseResumeProductDto;
 use App\Dto\Response\ResponseResumeProductVariantDto;
 use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
 
@@ -12,29 +10,20 @@ class ResponseResumeProductVariantDtoNormalizer implements NormalizerInterface
     public function normalize($object, $format = null, array $context = []): array
     {
         return [
-            'title' => $object->responseResumeProductDto->title,
-            'description' => $object->description,
-            'type' => $object->responseResumeProductDto->type->value,
-            'category' => [
-                'name' => $object->responseResumeProductDto->category->name,
-                'publicId' => $object->responseResumeProductDto->category->publicId->publicId,
-            ],
-            'unitPrice' => $object->responseResumeProductDto->unitPrice->amount,
-            'publicId' => $object->responseResumeProductDto->publicId->publicId,
-            'stock' => $object->stock,
-            'imageUrls' => $object->imageUrls,
-            //TODO: 'averageRating' => $object->averageRating,
-            'productCollection' => $object->otherProductVariants,
+            'publicId' => $object->publicId->publicId,
+            'unitPrice' => $object->unitPrice,
+            'wood' => $object->wood,
+            'imageUrl' => $object->imageUrl,
         ];
     }
 
     public function supportsNormalization($data, $format = null, array $context = []): bool
     {
-        return $data instanceof ResponseResumeProductDto;
+        return $data instanceof ResponseResumeProductVariantDto;
     }
 
     public function getSupportedTypes(?string $format): array
     {
-        return [ResponseResumeProductDto::class => true];
+        return [ResponseResumeProductVariantDto::class => true];
     }
 }
