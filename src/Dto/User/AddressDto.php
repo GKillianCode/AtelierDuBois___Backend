3b67e10e932d0d122d37443f1ab3aefa01d0ffba
Killian Godet <godetkillian@outlook.com>
2026-02-15 17:24:28 +0100

    refactor(cleancode01): renommer, ajouter getters
    - Renommer : de ResponseAddressDto => AddressDto
    - Ajouter : possibilité que publicIdDto soit null
    - Propriété de public => privé + getters

---
+++ src/Dto/User/AddressDto.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Dto\User;
+
+use App\Dto\Types\PublicIdDto;
+
+class AddressDto
+{
+    public function __construct(
+        private ?PublicIdDto $publicId,
+        private readonly string $street,
+        private readonly string $city,
+        private readonly string $zipcode,
+        private readonly bool $isProfessionnal,
+        private readonly bool $isDefault
+    ) {}
+
+    public function getPublicId(): ?PublicIdDto
+    {
+        return $this->publicId;
+    }
+
+    public function getStreet(): string
+    {
+        return $this->street;
+    }
+
+    public function getCity(): string
+    {
+        return $this->city;
+    }
+
+    public function getZipcode(): string
+    {
+        return $this->zipcode;
+    }
+
+    public function isProfessionnal(): bool
+    {
+        return $this->isProfessionnal;
+    }
+
+    public function isDefault(): bool
+    {
+        return $this->isDefault;
+    }
+}
