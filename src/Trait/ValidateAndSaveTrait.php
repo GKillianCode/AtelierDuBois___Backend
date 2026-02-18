bbb0a1085b97b801e82b4c51f7894359eec69dcc
Killian Godet <godetkillian@outlook.com>
2026-02-18 11:14:33 +0100

    fix(cleancode01): supprimer constructeur et récupérer type d'entité pour les logs

--- src/Trait/ValidateAndSaveTrait.php
+++ src/Trait/ValidateAndSaveTrait.php
@@ -2,28 +2,24 @@
 
 namespace App\Trait;
 
-use App\Util\ValidatorUtil;
-use Psr\Log\LoggerInterface;
-use Doctrine\ORM\EntityManagerInterface;
-
 trait ValidateAndSaveTrait
 {
-    public function __construct(
-        private readonly EntityManagerInterface $entityManager,
-        private readonly LoggerInterface $logger,
-        private readonly ValidatorUtil $validatorUtil
-    ) {}
-
     public function validateAndSave(object $entity): void
     {
         $this->logger->debug("ValidateAndSaveTrait::validateAndSave ENTER");
+
         $violations = $this->validatorUtil->getViolationsAsArray($entity);
         if (!empty($violations)) {
-            $this->logger->error("ValidateAndSaveTrait::validateAndSave VALIDATION ERROR");
-            throw new \RuntimeException('Validation error while adding address: ' . json_encode($violations));
+            $this->logger->error("ValidateAndSaveTrait::validateAndSave VALIDATION ERROR", [
+                'entity' => \get_class($entity),
+                'violations' => $violations
+            ]);
+            throw new \InvalidArgumentException('Validation error: ' . json_encode($violations));
         }
+
         $this->entityManager->persist($entity);
         $this->entityManager->flush();
+
         $this->logger->debug("ValidateAndSaveTrait::validateAndSave EXIT");
     }
 }
