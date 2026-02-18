472be2399f089d042b21a8e2d02cf63d2314f79e
Killian Godet <godetkillian@outlook.com>
2026-02-18 17:48:25 +0100

    refactor(cleancode01): changement des propriétés publique en privé et ajout de getters

---
+++ src/Dto/Request/RegisterUserDto.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Dto\Request;
+
+class RegisterUserDto
+{
+    public function __construct(
+        private readonly string $firstname,
+        private readonly string $lastname,
+        private readonly string $email,
+        private readonly string $password,
+    ) {}
+
+    public function getFirstname(): string
+    {
+        return $this->firstname;
+    }
+
+    public function getLastname(): string
+    {
+        return $this->lastname;
+    }
+
+    public function getEmail(): string
+    {
+        return $this->email;
+    }
+
+    public function getPassword(): string
+    {
+        return $this->password;
+    }
+}
