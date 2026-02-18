e9ff532e97259b7c7052ef295a4b113fdd7106c6
Killian Godet <godetkillian@outlook.com>
2026-02-18 11:29:45 +0100

    fix(cleancode01): génère un nouvel publicId uniquement si le dto n'en contient pas

--- src/Mapper/User/AddressMapper.php
+++ src/Mapper/User/AddressMapper.php
@@ -8,8 +8,6 @@ use App\Dto\User\AddressDto;
 use App\Entity\User\Address;
 use Psr\Log\LoggerInterface;
 use App\Dto\Types\PublicIdDto;
-use App\Dto\Register\RegisterAddressDto;
-use App\Dto\Response\ResponseAddressDto;
 
 class AddressMapper
 {
@@ -22,11 +20,9 @@ class AddressMapper
     {
         $this->logger->debug("AddressMapper::toEntityFromDto ENTER");
 
-        $newUuidBase62 = $this->uuidUtil->generateUuid62();
-
         $address = new Address();
         $address->setUserId($user)
-            ->setPublicId($newUuidBase62)
+            ->setPublicId($addressDto->getPublicId() ? $addressDto->getPublicId()->getPublicId() : $this->uuidUtil->generateUuid62())
             ->setStreet($addressDto->getStreet())
             ->setZipcode($addressDto->getZipcode())
             ->setCity($addressDto->getCity())
