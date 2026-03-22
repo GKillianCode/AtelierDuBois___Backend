3cf0795e517bf0856899d0e05fd3fdf22c2e57c5
Killian Godet <godetkillian@outlook.com>
2026-03-22 08:57:05 +0100

    feat(history): ajout d'un dto qui regroupe les filtres pour la récupération de l'historique
    - Page
    - Limite d'item par page
    - Recherche
    - Filtre de trie (ShipmentHistorySortFilterCode)
    - Category

---
+++ src/Dto/Request/Filter/GetShipmentHistoryRequestDto.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Dto\Request\Filter;
+
+use App\Dto\Types\PublicIdDto;
+use App\Enum\SortFilter\ShipmentHistorySortFilterCode;
+
+class GetShipmentHistoryRequestDto
+{
+    public function __construct(
+        private int $page,
+        private int $limit,
+        private string $search,
+        private ShipmentHistorySortFilterCode $filter,
+        private ?PublicIdDto $categoryPublicId,
+    ) {}
+
+    public function getPage(): int
+    {
+        return $this->page;
+    }
+
+    public function getLimit(): int
+    {
+        return $this->limit;
+    }
+
+    public function getSearch(): string
+    {
+        return $this->search;
+    }
+
+    public function getFilter(): ShipmentHistorySortFilterCode
+    {
+        return $this->filter;
+    }
+
+    public function getCategoryPublicId(): ?PublicIdDto
+    {
+        return $this->categoryPublicId;
+    }
+}
