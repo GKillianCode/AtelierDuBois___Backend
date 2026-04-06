6d3e9f3b2bb84be8745598c5ed014480198adba9
Killian Godet <godetkillian@outlook.com>
2026-04-06 19:48:36 +0200

    feat(shipment): ajout d'une méthode getNextStatus qui prend en compte les 2 types de produit

--- src/Enum/ShipmentStatusCode.php
+++ src/Enum/ShipmentStatusCode.php
@@ -5,6 +5,7 @@ namespace App\Enum;
 enum ShipmentStatusCode: string
 {
     case PENDING = 'PENDING';
+    case IN_BUILD = 'IN_BUILD';
     case IN_TRANSIT = 'IN_TRANSIT';
     case OUT_FOR_DELIVERY = 'OUT_FOR_DELIVERY';
     case DELIVERED = 'DELIVERED';
@@ -18,6 +19,7 @@ enum ShipmentStatusCode: string
     {
         return match ($this) {
             self::PENDING => 'En attente de traitement',
+            self::IN_BUILD => 'En cours de fabrication',
             self::IN_TRANSIT => 'En transit',
             self::OUT_FOR_DELIVERY => 'En cours de livraison',
             self::DELIVERED => 'Livrée',
@@ -32,5 +34,19 @@ enum ShipmentStatusCode: string
     public static function getFirstStatus(): self
     {
         return self::PENDING;
+    }
+
+    public static function getNextStatus(self $currentStatus, ProductType $productType): ?self
+    {
+        return match ($currentStatus) {
+            self::PENDING => match ($productType) {
+                ProductType::IN_STOCK => self::IN_TRANSIT,
+                ProductType::CUSTOM_MADE => self::IN_BUILD,
+            },
+            self::IN_BUILD => self::IN_TRANSIT,
+            self::IN_TRANSIT => self::OUT_FOR_DELIVERY,
+            self::OUT_FOR_DELIVERY => self::DELIVERED,
+            default => null,
+        };
     }
 }
