889b35d7477c04c54c0406d80b29986ce9debd3c
Killian Godet <godetkillian@outlook.com>
2026-05-01 17:51:03 +0200

    feat(shipmentDetails): ajout de la propriété shipmentId

--- src/Dto/Response/ResponseShipmentsHistoryDto.php
+++ src/Dto/Response/ResponseShipmentsHistoryDto.php
@@ -10,7 +10,8 @@ class ResponseShipmentsHistoryDto
         /** @var ResponseShipmentItemDto[] */
         private array $shipments,
         private PriceDto $totalPriceInCents,
-        private int $orderedAt
+        private int $orderedAt,
+        private string $shipmentId,
     ) {}
 
     /** @return ResponseShipmentItemDto[] */
@@ -27,5 +28,10 @@ class ResponseShipmentsHistoryDto
     public function getOrderedAt(): int
     {
         return $this->orderedAt;
+    }
+
+    public function getShipmentId(): string
+    {
+        return $this->shipmentId;
     }
 }
